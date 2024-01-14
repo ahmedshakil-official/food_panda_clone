6de22026f746621a9a0e7d2bb182ce487244b8a0
Shakil Ahmed <ahmedshakil.official@gmail.com>
2024-01-15 02:14:56 +0600

    Added custom user model and created user profile.

--- food_panda_clone/settings.py
+++ food_panda_clone/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core.apps.CoreConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -126,7 +128,12 @@ STATICFILES_DIRS = [
     BASE_DIR / "food_panda_clone/static"
 ]
 
+# Media files config
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "accounts.User"
